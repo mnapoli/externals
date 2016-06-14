3d57179a65244f10199d4b398102ccf636ecf27b
Matthieu Napoli <matthieu@mnapoli.fr>
2016-06-14 21:29:05 +0200

    Show the email count in each thread

--- src/Domain/Thread/ThreadRepository.php
+++ src/Domain/Thread/ThreadRepository.php
@@ -3,19 +3,59 @@ declare(strict_types = 1);
 
 namespace Externals\Domain\Thread;
 
+use Doctrine\DBAL\Connection;
+use Externals\Domain\NotFound;
+
 /**
  * @author Matthieu Napoli <matthieu@mnapoli.fr>
  */
-interface ThreadRepository
+class ThreadRepository
 {
     /**
-     * @return int|null Thread ID or null if not found.
+     * @var Connection
      */
-    public function findBySubject(string $subject);
+    private $db;
 
-    public function getSubject(int $id) : string;
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
 
-    public function create(string $subject) : int;
+    public function findBySubject(string $subject)
+    {
+        $threadId = $this->db->fetchColumn('SELECT id FROM threads WHERE subject = ?', [$subject]);
 
-    public function findLatest() : array;
+        return $threadId ? (int) $threadId : null;
+    }
+
+    public function getSubject(int $id) : string
+    {
+        $subject = $this->db->fetchColumn('SELECT subject FROM threads WHERE id = ?', [$id]);
+
+        if (!$subject) {
+            throw new NotFound('No thread found for ID ' . $id);
+        }
+
+        return $subject;
+    }
+
+    public function create(string $subject) : int
+    {
+        $this->db->insert('threads', [
+            'subject' => $subject,
+        ]);
+
+        return (int) $this->db->lastInsertId();
+    }
+
+    public function findLatest() : array
+    {
+        $query = 'SELECT threads.id, threads.subject, COUNT(emails.id) as emailCount, MAX(emails.date) as lastUpdate
+            FROM threads
+            LEFT JOIN emails ON threads.id = emails.threadId
+            GROUP BY threads.id
+            ORDER BY lastUpdate DESC
+            LIMIT 30';
+        return $this->db->fetchAll($query);
+    }
 }
