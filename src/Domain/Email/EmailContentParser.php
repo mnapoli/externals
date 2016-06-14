aed66c2635b1451f4becb60b6f7588729cb1c899
Matthieu Napoli <matthieu@mnapoli.fr>
2016-06-14 21:39:50 +0200

    Turn text links into clickable links

--- src/Domain/Email/EmailContentParser.php
+++ src/Domain/Email/EmailContentParser.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace Externals\Domain\Email;
 
+use Misd\Linkify\Linkify;
+
 /**
  * @author Matthieu Napoli <matthieu@mnapoli.fr>
  */
@@ -14,10 +16,24 @@ class EmailContentParser
     ];
     const QUOTE = '>';
 
+    /**
+     * @var Linkify
+     */
+    private $linkify;
+
+    public function __construct(Linkify $linkify)
+    {
+        $this->linkify = $linkify;
+    }
+
     public function parse(string $content) : string
     {
         $content = str_replace(self::FOOTER, '', $content);
         $content = trim($content, " \t\n\r\0\x0B->");
+
+        $content = $this->linkify->process($content, [
+            'attr' => ['rel' => 'nofollow'],
+        ]);
 
         $lines = preg_split('/\R/', $content); // explode all lines
 
