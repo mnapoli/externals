afe9e167e36584c64dc14d38d5d57a3240f09a38
Matthieu Napoli <matthieu@mnapoli.fr>
2017-07-04 20:58:06 +0200

    Hack to make the session with in dev environment without HTTPS

--- res/config/env/dev.php
+++ res/config/env/dev.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types = 1);
 
+use Dflydev\FigCookies\SetCookie;
+use Interop\Container\ContainerInterface;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use PSR7Session\Http\SessionMiddleware;
+use PSR7Session\Time\SystemCurrentTime;
 use function DI\get;
 
 return [
@@ -14,5 +20,22 @@ return [
     ],
 
     'algolia.index_prefix' => 'dev_',
+
+    // Allows using the session without HTTPS (for dev purposes)
+    SessionMiddleware::class => function (ContainerInterface $c) {
+        $config = $c->get('session');
+        return new SessionMiddleware(
+            new Sha256,
+            (string) $config['key'],
+            (string) $config['key'],
+            SetCookie::create(SessionMiddleware::DEFAULT_COOKIE)
+                ->withSecure(false) // THIS IS THE SECURE FLAG WE SET TO FALSE
+                ->withHttpOnly(true)
+                ->withPath('/'),
+            new Parser,
+            (int) $config['lifetime'],
+            new SystemCurrentTime
+        );
+    },
 
 ];
