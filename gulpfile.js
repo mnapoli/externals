7954d821b7566c41191983a631440a97d969a927
Markus Staab <maggus.staab@googlemail.com>
2019-02-09 08:13:39 +0100

    Drop bootstrap.js
    It seems we dont use it anywhere

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ const cleanCSS = require('gulp-clean-css');
 gulp.task('js', function() {
     return gulp.src([
         './node_modules/jquery/dist/jquery.js',
-        './node_modules/bootstrap/dist/js/bootstrap.js',
         './node_modules/moment/min/moment.min.js',
         './res/assets/main.js',
     ])
