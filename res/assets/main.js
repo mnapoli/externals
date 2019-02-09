7893876c9d4926fdc768535cf253f1aea346c3ed
Markus Staab <markus.staab@redaxo.de>
2019-02-09 13:20:12 +0100

    vanillajs collapse

--- res/assets/main.js
+++ res/assets/main.js
@@ -38,3 +38,29 @@ function vote($voteLink, value) {
         alert('An error occurred');
     });
 }
+
+// map our commands to the classList methods
+const fnmap = {
+    'toggle': 'toggle',
+    'show': 'add',
+    'hide': 'remove'
+};
+
+const collapse = (selector, cmd) => {
+    const targets = Array.from(document.querySelectorAll(selector));
+    targets.forEach(target => {
+        target.classList[fnmap[cmd]]('show');
+    });
+}
+
+// Grab all the trigger elements on the page
+const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+// Listen for click events, but only on our triggers
+window.addEventListener('click', (ev) => {
+    const elm = ev.target;
+    if (triggers.includes(elm)) {
+        const selector = elm.getAttribute('data-target');
+        collapse(selector, 'toggle');
+    }
+}, false);
