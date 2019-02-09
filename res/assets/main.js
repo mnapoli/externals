b48859675d6648299f48488795d7b0184a6b6bb4
Markus Staab <markus.staab@redaxo.de>
2019-02-09 13:24:46 +0100

    use ES5

--- res/assets/main.js
+++ res/assets/main.js
@@ -40,27 +40,27 @@ function vote($voteLink, value) {
 }
 
 // map our commands to the classList methods
-const fnmap = {
+var fnmap = {
     'toggle': 'toggle',
     'show': 'add',
     'hide': 'remove'
 };
 
-const collapse = (selector, cmd) => {
-    const targets = Array.from(document.querySelectorAll(selector));
+var collapse = (selector, cmd) => {
+    var targets = Array.from(document.querySelectorAll(selector));
     targets.forEach(target => {
         target.classList[fnmap[cmd]]('show');
     });
 }
 
 // Grab all the trigger elements on the page
-const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+var triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
 
 // Listen for click events, but only on our triggers
 window.addEventListener('click', (ev) => {
-    const elm = ev.target;
+    var elm = ev.target;
     if (triggers.includes(elm)) {
-        const selector = elm.getAttribute('data-target');
+        var selector = elm.getAttribute('data-target');
         collapse(selector, 'toggle');
     }
 }, false);
