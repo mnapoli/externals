0860eb6593470504834877eee8a3fb6710f1a8a1
Matthieu Napoli <matthieu@mnapoli.fr>
2020-11-23 12:50:35 +0100

    Revert "Debug Bref 1.0"
    This reverts commit ba57f738

--- src/Application/Controller/NotFoundController.php
+++ src/Application/Controller/NotFoundController.php
@@ -4,7 +4,6 @@ namespace Externals\Application\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Log\LoggerInterface;
 use Twig\Environment;
 use Zend\Diactoros\Response\HtmlResponse;
 
@@ -12,24 +11,14 @@ class NotFoundController
 {
     /** @var Environment */
     private $twig;
-    /** @var LoggerInterface */
-    private LoggerInterface $logger;
 
-    public function __construct(Environment $twig, LoggerInterface $logger)
+    public function __construct(Environment $twig)
     {
         $this->twig = $twig;
-        $this->logger = $logger;
     }
 
     public function __invoke(ServerRequestInterface $request): ResponseInterface
     {
-        $this->logger->error('Not found: ' . $request->getUri() . PHP_EOL . json_encode([
-            '$request->getServerParams()' => $request->getServerParams(),
-            '$request->getUri()->getPath()' => $request->getUri()->getPath(),
-            '$request->getUri()->getHost()' => $request->getUri()->getHost(),
-            '$_SERVER' => $_SERVER,
-        ]));
-
         newrelic_name_transaction('404');
         $response = new HtmlResponse($this->twig->render('404.html.twig', [
             'user' => $request->getAttribute('user'),
