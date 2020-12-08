cb09f209207d48525f9eec9957ee4e7c409b754a
Matthieu Napoli <matthieu@mnapoli.fr>
2020-12-08 22:24:12 +0100

    Try lift

---
+++ lift/index.js
@@ -0,0 +1,36 @@
+const child_process = require('child_process');
+
+class LiftPlugin {
+    constructor(serverless, options) {
+        this.serverless = serverless;
+
+        this.setEnvironmentVariables();
+        this.setPermissions();
+    }
+
+    setEnvironmentVariables() {
+        this.serverless.service.provider.environment = this.serverless.service.provider.environment || {};
+
+        const json = child_process.execSync('lift-dev variables');
+        const variables = JSON.parse(json.toString());
+
+        Object.keys(variables).map(name => {
+            if (name in this.serverless.service.provider.environment) {
+                // Avoid overwriting an existing variable
+                return;
+            }
+            this.serverless.service.provider.environment[name] = variables[name];
+        });
+    }
+
+    setPermissions() {
+        this.serverless.service.provider.iamRoleStatements = this.serverless.service.provider.iamRoleStatements || [];
+
+        const json = child_process.execSync('lift-dev permissions');
+        const permissions = JSON.parse(json.toString());
+
+        this.serverless.service.provider.iamRoleStatements.push(...permissions);
+    }
+}
+
+module.exports = LiftPlugin;
