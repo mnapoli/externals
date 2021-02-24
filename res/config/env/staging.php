3ad0d18ce3d16dd7bf770311a6bde34a4cf1a39f
Matthieu Napoli <matthieu@mnapoli.fr>
2021-02-24 23:10:09 +0100

    Fix assets URL

--- res/config/env/staging.php
+++ res/config/env/staging.php
@@ -11,7 +11,7 @@ return [
     'sentry.url' => null,
 
     // Assets will not be proxied by Cloudfront, instead we use the direct S3 URL
-    'assetsBaseUrl' => 'https://' . $_SERVER['ASSETS_BUCKET_DOMAIN'],
+    'assetsBaseUrl' => 'https://' . $_SERVER['BUCKET_ASSETS'] . '.s3-eu-west-1.amazonaws.com',
 
     // Disable indexing in Algolia
     SearchIndex::class => create(ReadOnlySearchIndex::class),
