c5b8ec5abb33cc004f14312e63f8672c502a9401
Matthieu Napoli <matthieu@mnapoli.fr>
2026-06-02 13:54:49 +0900

    Configure Sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +17,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
